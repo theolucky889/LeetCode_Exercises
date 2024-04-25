ad8307ef8ef3ea3401ab9da41adbf443422cb932
theolucky889 <theolucky889@gmail.com>
2024-04-25 14:49:20 +0800

    Update 1051._Height_Checker.py

--- Easy/1051._Height_Checker.py
+++ Easy/1051._Height_Checker.py
@@ -1,3 +1,49 @@
+'''
+1051. Height Checker
+Easy
+Topics
+Companies
+Hint
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+ 
+
+Example 1:
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+Example 2:
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+Example 3:
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+ 
+
+Constraints:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+'''
+
 class Solution(object):
     def heightChecker(self, heights):
         """
@@ -9,7 +55,9 @@ class Solution(object):
         # returns the total of number of position where heights(h1) is not equal to expected(h2)
         return sum(h1 != h2 for h1, h2 in zip(heights, expected))   # zip() = create pairs of heights and expected heights to compare
 
+# call out class Solution for output
 solution = Solution()
+
 print(solution.heightChecker([1,1,4,2,1,3]))
 print(solution.heightChecker([5,1,2,3,4]))
 print(solution.heightChecker([1,2,3,4,5]))
