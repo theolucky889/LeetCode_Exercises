ace0cfc1577b59ee0bdce11adc87d496b9361889
theolucky889 <theolucky889@gmail.com>
2024-05-01 23:49:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Easy.Height_Checker import Height_Checker  # import module the height checker function from height checker file
 from Medium.Permutations import Permutations    # import module permutation function from permutation file
+from Medium.CombinationSum import CombSum
 
 def main():
     # method to callout the function so it will be called later in the loop
@@ -28,6 +29,18 @@ def main():
     for nums in nums_list:
         print(perm.permute(nums))
 
+    combSum = CombSum()
+    candidate_list = [
+        [2,3,6,7],
+        [2,3,5],
+        [2]
+    ]
+    targets = [7, 8, 1]
+    print('Combination Sum: ')
+    for candidates, target in zip(candidate_list,targets):
+        print(f'For candidates {candidates} with target {target}')
+        result = (combSum.combinationSum(candidates, target))
+        print(result)
 # run the main file
 if __name__ == '__main__':  # this function is used so that only in the main function will be run (not the modules + main())also to make distinctions between files
     main()
