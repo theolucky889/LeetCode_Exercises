2578ce5ef1fc7fd95773ae4152f12e2c2716376f
theolucky889 <theolucky889@gmail.com>
2024-05-05 00:28:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Easy.Height_Checker import Height_Checker  # import module the height checker function from height checker file
 from Medium.Permutations import Permutations    # import module permutation function from permutation file
 from Medium.CombinationSum import CombSum
+from Hard.N_Queen import Queen
 
 def main():
     # method to callout the function so it will be called later in the loop
@@ -29,18 +30,33 @@ def main():
     for nums in nums_list:
         print(perm.permute(nums))
 
-    # combSum = CombSum()
-    # candidate_list = [
-    #     [2,3,6,7],
-    #     [2,3,5],
-    #     [2]
-    # ]
-    # targets = [7, 8, 1]
-    # print('Combination Sum: ')
-    # for candidates, target in zip(candidate_list,targets):
-    #     print(f'For candidates {candidates} with target {target}')
-    #     result = (combSum.combinationSum(candidates, target))
-    #     print(result)
+    combSum = CombSum()
+    candidate_list = [
+        [2,3,6,7],
+        [2,3,5],
+        [2]
+    ]
+    targets = [7, 8, 1]
+    print('Combination Sum: ')
+    for candidates, target in zip(candidate_list,targets):
+        print(f'For candidates {candidates} with target {target}')
+        result = (combSum.combinationSum(candidates, target))
+        print(result)
+        
+    n_Queen = Queen()
+    queens = [
+        4,
+        5
+    ]
+    print('N-Queens Solution: ')
+    for n in queens:
+        print(f'\nSolutions for {n} Queens: ')
+        solutions = n_Queen.solveNQueens(n)
+        for solution in solutions:
+            for row in solution:
+                print(row)
+            print('')
+    
 # run the main file
 if __name__ == '__main__':  # this function is used so that only in the main function will be run (not the modules + main())also to make distinctions between files
     main()
